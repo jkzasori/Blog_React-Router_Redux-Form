38f067a8362b40947f53ad1ea01c23fa1eb4aac1
José Támara <jochitoel1@hotmail.com>
2018-03-06 16:22:51 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {render} from 'react-dom';
-import Home from './containers/home';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import reducers from './reducers';
+
+import PostsIndex from './components/posts_index';
 
 const app = document.getElementById("app");
+const createStoreWithMiddleware= applyMiddleware()(createStore);
 
-render(<Home />, app);
+render(
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<BrowserRouter>
+			<div>
+				<Route path="/" component={PostsIndex} />
+			</div>
+		</BrowserRouter>
+	</Provider>
+	, app)
