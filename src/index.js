c43b13d99fd0e4e0e0e2e37229f09824d65c142b
José Támara <jochitoel1@hotmail.com>
2018-03-06 18:43:21 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import {render} from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
-import reducers from './reducers';
+import promise from 'redux-promise';
 
+import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
 const app = document.getElementById("app");
-const createStoreWithMiddleware= applyMiddleware()(createStore);
+const createStoreWithMiddleware= applyMiddleware(promise)(createStore);
 
 render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
